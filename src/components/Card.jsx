8efc8d25b9c18f6a6e5ac1054d89703474c94816
bgdd2008 <m.bagdava@gmail.com>
2025-06-20 14:27:16 +0400

    Changed Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Card({title='Card title',content='Card content' , img = '/assets/pic.jpg'}) {
+export default function Card({title='Card title',desc='Card content' , img = '/assets/pic.jpg'}) {
   return (
     <div className="card">
         <h1>{title}</h1>
-        <p>{content}</p>
+        <p>{desc}</p>
         <img src={img} alt={img} width={150} height={150} />
       </div>
   )
